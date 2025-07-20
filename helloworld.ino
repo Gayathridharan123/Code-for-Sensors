2233f59080616e503bec66f8bc42b6d73484c26e
Gayathridharan123 <gayuganga20041977@gmail.com>
2025-07-20 20:28:48 +0530

    Add files via upload

---
+++ helloworld.ino
@@ -0,0 +1,18 @@
+#include<LiquidCrystal.h>
+const int rs =13 ,en=12,d4=11,d5=10,d6=9,d7=8;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+void setup()
+{
+  lcd.begin(16,2);
+  
+
+}
+
+void loop()
+{
+  lcd.setCursor(0,1);
+  lcd.print("HELLO WORLD");
+  lcd.print("    ");
+  
+  }
